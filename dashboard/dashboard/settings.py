7e67f9e9e806a2206c50bc9bf0d1bb2f714be841
Mina-Eissa <minaeissa268@gmail.com>
2023-06-18 00:07:42 +0300

    set username of database to 'Mina'

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dashboard',
-        'USER':'root',
+        'USER':'Mina',
         'PASSWORD':'1234',
         'HOST':'localhost',
         'PORT':'3306',
