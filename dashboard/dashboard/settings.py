8a6c7b6f0fcd27430aacecd269cce62dce420c63
Mina-Eissa <minaeissa268@gmail.com>
2023-06-17 23:55:27 +0300

    fix conflicts in settings.py

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -39,10 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api.apps.ApiConfig',
-<<<<<<< HEAD
-=======
     'corsheaders'
->>>>>>> feature/integrate-react
 ]
 
 MIDDLEWARE = [
